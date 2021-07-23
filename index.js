a54a5b26e965073372788db512df143fece8de78
Daniel Calvin <arockne2003@yahoo.com>
2021-07-23 09:52:21 -0700

    commit tree ebc8feb43da8619741f741166163c8793561d796

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//implement totalBatteries
+  //holds the sum of all of the batter amounts in batterBatches
+const totalBatteries = batteryBatches.reduce((a, b) => a + b);
